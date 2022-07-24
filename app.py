b973b6ac3971a98100bcc8b11a0f9771b795b3d5
parth-bh <105191524+parth-bh@users.noreply.github.com>
2022-07-24 23:23:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write("""# To Check Input is EVEN OR ODD""")
 st.header('User Input Parameters')
 
 def user_input_features():
-    number = st.number_input("Enter the Positive Number",min_value=0,max_value=100000000 ,step=1)
+    number = st.number_input("Enter the Positive Number",min_value=0,max_value=1000000000000000000000000000000000 ,step=1)
     result=None
     if st.button("Check"):
         result = "Even" if number%2 == 0 else "Odd"
