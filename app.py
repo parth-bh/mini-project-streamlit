4918cd042a0c317eac75c173a993b8bfca3e3460
parth-bh <105191524+parth-bh@users.noreply.github.com>
2022-07-24 22:33:08 +0530

    Add files via upload
    version1

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+def main():
+  st.title("To check the given number is Even or Odd")
+  html_temp = """
+  <div style="background-color:White;padding:10px">
+  <h2 style="color:Green;text-align:center;">Streamlit Even / Odd App <h2>
+  </div>
+  """
+  st.markdown(html_temp,unsafe_allow_html=True)
+  number = st.number_input("Enter a positive number",min_value=0,max_value=1000000000000,step=1)
+  result = None
+  if st.button("Check"):
+    result = "Even" if number%2 == 0 else "Odd"
+  st.success(f"The given number is {result}")
+  
+  if __name__=="__main__":
+    main()
