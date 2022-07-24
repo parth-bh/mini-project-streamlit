c3d7f93c508cafad87b7a089b31f5ad26bfb7b72
parth-bh <105191524+parth-bh@users.noreply.github.com>
2022-07-24 23:16:03 +0530

    v2

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 import streamlit as st
 
+import pickle
 
-def main():
-  st.title("Even or Odd")
-  html_temp = """
-  <div style="background-color:tomato;padding:10px">
-  <h2 style="color:white;text-align:center;">Streamlit Even / Odd App <h2>
-  </div>
-  """
-  st.markdown(html_temp,unsafe_allow_html=True)
-  number = st.number_input("Enter a positive number",min_value=0,max_value=1000000000000,step=1)
-  result = None
-  if st.button("Check"):
-    result = "Even" if number%2 == 0 else "Odd"
-  st.success(f"The given number is {result}")
-  
-  if __name__=="__main__":
-    main()
+st.write("""# To Check Input is EVEN OR ODD""")
+
+st.header('User Input Parameters')
+
+def user_input_features():
+    number = st.number_input("Enter the Positive Number",min_value=0,max_value=100000000 ,step=1)
+    result=None
+    if st.button("Check"):
+        result = "Even" if number%2 == 0 else "Odd"
+    st.success(f"The given number is {result}")
+    return number
+user_input_features()
